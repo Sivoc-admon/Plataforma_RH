12648e8524499cf05cb4a6ddea29ab72c3a26275
Imanol57864 <alfonsochoxd@gmail.com>
2025-08-08 09:14:42 -0600

    KF-01 testing on droplet completed.

--- playwright.config.js
+++ playwright.config.js
@@ -1,21 +1,25 @@
-// playwright.config.js
-// npx playwright test --workers=18
-// npx playwright test --reporter=html
-// npx playwright show-report
-// recuerda correr un solo worker con la camara on y headless false para debuggear
+/* playwright.config.js
+npx playwright test
+npx playwright test --reporter=html
+npx playwright show-rep ort
+page.pause(): Agrega await page.pause() justo antes de la línea que da el error. 
+Esto detendrá la ejecución de la prueba y abrirá el Inspector de Playwright, 
+permitiéndote interactuar con el navegador y ver el estado de la página en ese momento.
+
+const HOST = '143.198.232.196';
+// Usa la combined URL http://(ip)/(nginx_tag)/(url_tag)
+
+*/
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  workers: 18, // 35 total users // 18 (50%) = quality gate
   // Solo Chromium
   projects: [
     {
       name: 'chromium',
-      use: {
-        browserName: 'chromium',
-        headless: true
-      },
+      use: { browserName: 'chromium' }
     },
   ],
-
   // Configuración global de entorno
   use: {
     viewport: { width: 1280, height: 720 },
--- tests/KF-01/KF-01.spec.js
+++ tests/KF-01/KF-01.spec.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 const { test, expect } = require('@playwright/test');
 const credentials = require('./credentials.json');
 
-const BASE_URL = 'http://localhost:7613/modulorrhh';
+const LOCALHOST = 'localhost';
+const HOST = process.env.HOST;
+const COMBINED_URL = '/modulorrhh/urltagdiff';
+const BASE_URL = `http://${HOST}${COMBINED_URL}`;
+// Usa la combined URL http://(ip)/(nginx_tag)/(url_tag)
 
 test.describe.configure({ mode: 'parallel' });
 
 test.describe('tests', () => {
-
   for (const USER of credentials) { // Generamos N tests por usuario
-
     // Crear un describe anidado para cada usuario con modo serial
     test.describe(`Usuario ${USER.email}`, () => {
       test.describe.configure({ mode: 'serial' });
@@ -19,15 +22,12 @@ test.describe('tests', () => {
         await page.fill('input[name="password"]', USER.password);
         await page.check('input[name="remember"]');
         await page.click('button[type="submit"]');
-
         const bodyText = await page.textContent('body');
         const errorDetected = bodyText?.includes('[System Error]');
         if (errorDetected) {
           throw new Error('Error de sistema detectado en la página');
         }
-
         await page.waitForURL(`${BASE_URL}/inicio`);
-        await page.waitForTimeout(2000);
       });
 
       test(`Login into Tabla de usuarios de ${USER.email}`, async ({ page }) => {
@@ -42,7 +42,7 @@ test.describe('tests', () => {
         const menuIconSelector = 'svg[onclick="toggleSidebar()"]';
         await page.waitForSelector(menuIconSelector, { state: 'visible' });
         await page.click(menuIconSelector);
-        const usuariosLinkSelector = 'a[href="/modulorrhh/usuarios"]';
+        const usuariosLinkSelector = `a[href="${COMBINED_URL}/usuarios"]`;
 
         // Intentamos encontrar el enlace y acceder con diferentes roles de usuario
         await page.waitForTimeout(1000); // esperar máximo 1seg en lo que carga la sidebar
@@ -75,11 +75,12 @@ test.describe('tests', () => {
         const menuIconSelector = 'svg[onclick="toggleSidebar()"]';
         await page.waitForSelector(menuIconSelector, { state: 'visible' });
         await page.click(menuIconSelector);
-        const logoutLinkSelector = 'a[href="/modulorrhh/logout"]';
-
+        const logoutLinkSelector = `a[href="${COMBINED_URL}/logout"]`;
+      
         // Intentamos hacer logout accediendo al botón de cerrar sesión
         await page.waitForSelector(logoutLinkSelector, { state: 'visible' });
         await page.click(logoutLinkSelector);
+        await page.waitForTimeout(1000); // esperar máximo 1seg para revisar el resultado
 
         // Ahora intentamos ingresar a una vista protegida y debería darnos http 401
         const response = await page.goto(`${BASE_URL}/inicio`);
